c0aa53e6640d689c540c80d052b64e3567841e75
Evelyn Kumeh <evelynkumeh@Evelyns-Air.lan>
2025-04-02 20:25:54 -0500

    Started decoder function

--- decoder/decoder.js
+++ decoder/decoder.js
@@ -6,8 +6,10 @@ let output1 = document.getElementById("code-output");
 console.log(button, " is the button.");
 
 // defines the function
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 function encoder1(){
-    let value = input1.value;
+    let val_ = input1.value;
+    let value = val_.toUpperCase()
     console.log("INPUT IS: ", value);
 
 
@@ -15,8 +17,6 @@ function encoder1(){
     console.log(" The word is ", len, " letters long.");
 
     let encodedValue = "";
-
-    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
     for(let i=0;i<len; i++) {
         console.log("the letter is ", value[i]);
@@ -26,6 +26,27 @@ function encoder1(){
         encodedValue += result +"."; 
     }
     output1.innerHTML = encodedValue; 
+}
+
+//user input word is decoded 
+let input2 = document.getElementById("word2");
+let button2 = document.getElementById("button2")
+let output2 = document.getElementById("code-output2")
+
+//decoder 1 
+function decoder1(){
+    let val_ = input2.value;
+    console.log(val_, " is the value");
+
+    let len = val_.length;
+    console.log(" The variable len is ", len);
+
+
+    for(let i=0;i<len; i++ ) {
+        console.log("the index is ", i);
+        console.log("when the value is ", val_[i]);
+    }
+
 }
 
 //function call
